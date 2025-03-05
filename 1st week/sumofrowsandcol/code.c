5e84be2c75d02b63b39a90593101eca1603a5c7a
kavyasingh03 <kavyasingh.cs23@bmsce.ac.in>
2025-03-05 12:08:38 +0530

    code.c

---
+++ 1st week/sumofrowsandcol/code.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+    
+    printf("Enter the number of rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+    
+    int matrix[rows][cols];
+    
+    printf("Enter the elements of the matrix:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    printf("Sum of each row:\n");
+    for (int i = 0; i < rows; i++) {
+        int rowSum = 0;
+        for (int j = 0; j < cols; j++) {
+            rowSum += matrix[i][j];
+        }
+        printf("Row %d: %d\n", i + 1, rowSum);
+    }
+
+    printf("Sum of each column:\n");
+    for (int j = 0; j < cols; j++) {
+        int colSum = 0;
+        for (int i = 0; i < rows; i++) {
+            colSum += matrix[i][j];
+        }
+        printf("Column %d: %d\n", j + 1, colSum);
+    }
+    
+    return 0;
+}
