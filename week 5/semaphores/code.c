f8bf1ccf04dc2005b6014eebfd80044cbce0751a
kavyasingh03 <kavyasingh.cs23@bmsce.ac.in>
2025-04-02 12:37:06 +0530

    code.c

--- week 5/semaphores/code.c
+++ week 5/semaphores/code.c
@@ -6,28 +6,41 @@
 #define BUFFER_SIZE 5
 
 sem_t full, empty;
+pthread_mutex_t mutex;
 int x = 0;
 int buffer[BUFFER_SIZE];
 int count = 0;
 
-void wait_sem() {
-    if (count == 0) {
+void wait_sem()
+{
+    pthread_mutex_lock(&mutex);
+    if (count == 0)
+    {
         printf("Buffer is empty!\n");
+        pthread_mutex_unlock(&mutex);
         return;
     }
     count--;
+    pthread_mutex_unlock(&mutex);
 }
 
-void signal_sem() {
-    if (count == BUFFER_SIZE) {
+void signal_sem()
+{
+    pthread_mutex_lock(&mutex);
+    if (count == BUFFER_SIZE)
+    {
         printf("Buffer is full!\n");
+        pthread_mutex_unlock(&mutex);
         return;
     }
     count++;
+    pthread_mutex_unlock(&mutex);
 }
 
-void producer() {
-    if (count == BUFFER_SIZE) {
+void producer()
+{
+    if (count == BUFFER_SIZE)
+    {
         printf("Buffer is full!\n");
         return;
     }
@@ -36,8 +49,10 @@ void producer() {
     printf("Producer has produced: Item %d\n", x);
 }
 
-void consumer() {
-    if (count == 0) {
+void consumer()
+{
+    if (count == 0)
+    {
         printf("Buffer is empty!\n");
         return;
     }
@@ -46,16 +61,21 @@ void consumer() {
     wait_sem();
 }
 
-int main() {
+int main()
+{
     int choice;
     sem_init(&full, 0, 0);
     sem_init(&empty, 0, BUFFER_SIZE);
+    pthread_mutex_init(&mutex, NULL);
+    
     printf("\nEnter 1.Producer 2.Consumer 3.Exit\n");
-    while (1) {
+    while (1)
+    {
         printf("Enter your choice: ");
         scanf("%d", &choice);
         
-        switch (choice) {
+        switch (choice)
+        {
             case 1:
                 producer();
                 break;
@@ -63,13 +83,12 @@ int main() {
                 consumer();
                 break;
             case 3:
+                pthread_mutex_destroy(&mutex);
+                sem_destroy(&full);
+                sem_destroy(&empty);
                 return 0;
             default:
                 printf("Invalid choice!\n");
         }
     }
-    
-    sem_destroy(&full);
-    sem_destroy(&empty);
-    return 0;
 }
